f0ae7a8ec2cbbf671f39cbe84700d6369ed4f93f
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-02-13 23:57:38 +0000

    vskernels: bring back zimg kernels
    A partial revert of d3a474c. These kernels were rewritten to be custom
    kernels in order to implement blur, because at the time we didn't have
    support for it through zimg, but now we do via resize2 after
    https://github.com/Jaded-Encoding-Thaumaturgy/vapoursynth-resize2/commit/8c3a6beab427848af00322dc223e71308bdf9641.
    
    With this, the Bicubics, Bilinear, Lanczos, Point, and (the relevant)
    Spline kernels are back to using resize2/zimg. There are still some
    custom kernels like generic Spline, Gaussian, Box, etc., which used to
    be implemented via fmtconv, but this patch is just focusing on what can
    be done in zimg today.

--- vskernels/kernels/__init__.py
+++ vskernels/kernels/__init__.py
@@ -5,5 +5,7 @@ from .bicubic import *
 from .complex import *
 from .custom import *
 from .placebo import *
+from .resize import *
 from .spline import *
 from .various import *
+from .zimg import *
--- vskernels/kernels/helpers.py
+++ vskernels/kernels/helpers.py
@@ -1,38 +1,10 @@
-from math import floor, pi, sin
+from __future__ import annotations
+
+from math import pi, sin
 
 __all__ = [
-    'sinc', 'poly3', 'round_halfup', 'bic_vals'
+    'sinc', 'poly3'
 ]
-
-
-class bic_vals:
-    @staticmethod
-    def p0(b: float, c: float) -> float:
-        return (6.0 - 2.0 * b) / 6.0
-
-    @staticmethod
-    def p2(b: float, c: float) -> float:
-        return (-18.0 + 12.0 * b + 6.0 * c) / 6.0
-
-    @staticmethod
-    def p3(b: float, c: float) -> float:
-        return (12.0 - 9.0 * b - 6.0 * c) / 6.0
-
-    @staticmethod
-    def q0(b: float, c: float) -> float:
-        return (8.0 * b + 24.0 * c) / 6.0
-
-    @staticmethod
-    def q1(b: float, c: float) -> float:
-        return (-12.0 * b - 48.0 * c) / 6.0
-
-    @staticmethod
-    def q2(b: float, c: float) -> float:
-        return (6.0 * b + 30.0 * c) / 6.0
-
-    @staticmethod
-    def q3(b: float, c: float) -> float:
-        return (-b - 6.0 * c) / 6.0
 
 
 def sinc(x: float) -> float:
@@ -41,7 +13,3 @@ def sinc(x: float) -> float:
 
 def poly3(x: float, c0: float, c1: float, c2: float, c3: float) -> float:
     return c0 + x * (c1 + x * (c2 + x * c3))
-
-
-def round_halfup(x: float) -> float:
-    return floor(x + 0.5) if x < 0 else floor(x + 0.49999999999999994)
