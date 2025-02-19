5ee242fe645b6690fa3ecc6f91db2c2752588fd7
arch1t3cht <arch1t3cht@gmail.com>
2025-02-19 13:19:46 +0100

    Bump version
    Cut one more stable with some bugfixes before bringing back
    the mvtools rework.

--- _metadata.py
+++ _metadata.py
@@ -1,6 +1,6 @@
 """Full suite of filters, wrappers, and helper functions for filtering video using VapourSynth"""
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __author_name__, __author_email__ = 'Jaded Encoding Thaumaturgy', 'jaded.encoding.thaumaturgy@gmail.com'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
