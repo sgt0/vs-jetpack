sgt0/vs-jetpack

commit d80ebec232f8fa4e8da2efd78802879961009a99

use norm_expr everywhere

vskernels/util.py
@@ -5,6 +5,7 @@ from math import exp
 from typing import Any, ClassVar, cast
 
 from jetpytools import inject_kwargs_params
+from vsexprtools import norm_expr
 from vstools import (
     ConstantFormatVideoNode, CustomRuntimeError, CustomValueError, HoldsVideoFormatT, InvalidTransferError, Matrix,
     MatrixT, Transfer, cachedproperty, depth, get_video_format, inject_self, to_singleton, vs
@@ -174,9 +175,10 @@ class LinearLight:
                         Transfer.from_video(wclip, self.__class__)
                     )
 
-                wclip = wclip.std.Expr(
-                    f'{self.ll._scenter} 1 {self.ll._sslope} / 1 x 0 max 1 min {self.ll._sscale} * '
-                    f'{self.ll._soffset} + / 1 - log * -'
+                wclip = norm_expr(
+                    '{center} 1 {slope} / 1 x 0 max 1 min {scale} * {offset} + / 1 - log * -',
+                    center=self.ll._scenter, slope=self.ll._sslope,
+                    scale=self.ll._sscale, offset=self.ll._soffset,
                 )
 
             return wclip
@@ -198,9 +200,10 @@ class LinearLight:
             processed = self._linear  # type: ignore
 
             if self.ll.sigmoid:
-                processed = processed.std.Expr(
-                    f'1 1 {self.ll._sslope} {self.ll._scenter} x 0 max 1 min - * exp + /'
-                    f' {self.ll._soffset} - {self.ll._sscale} /'
+                processed = norm_expr(
+                    '1 1 {slope} {center} x 0 max 1 min - * exp + / {offset} - {scale} /',
+                    slope=self.ll._sslope, center=self.ll._scenter,
+                    offset=self.ll._soffset, scale=self.ll._sscale,
                 )
 
             if self.ll.linear:

vsdeband/filters.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from functools import partial
 from math import sqrt
 
-from vsexprtools import complexpr_available, norm_expr, norm_expr_planes
+from vsexprtools import norm_expr
 from vskernels import Bilinear, Point, Scaler, ScalerT
 from vsrgtools import box_blur, gauss_blur
 from vstools import (
     ColorRange, ColorRangeT, PlanesT, check_ref_clip, check_variable, cround, depth, expect_bits, get_plane_sizes,
-    get_prop, normalize_planes, normalize_seq, vs
+    normalize_planes, normalize_seq, vs
 )
 
 from .types import GuidedFilterMode
@@ -105,39 +105,19 @@ def guided_filter(
 
         denominator = denominator.std.PlaneStats(None, 0)
 
-        if complexpr_available:
-            weight = norm_expr([weight_in, denominator], 'x 1e-06 + y.PlaneStatsAverage *', planes)
-        else:
-            weight = denominator.std.FrameEval(
-                lambda n, f: weight_in.std.Expr(
-                    norm_expr_planes(weight_in, f'x 1e-06 + {f.props.PlaneStatsAverage} *', planes)
-                ), denominator
-            )
+        weight = norm_expr([weight_in, denominator], 'x 1e-06 + y.PlaneStatsAverage *', planes)
 
         if mode is GuidedFilterMode.WEIGHTED:
             a = norm_expr([cov_Ip, var_I, weight], 'x y {thr} z / + /', planes, thr=thr)
         else:
             weight_in = weight_in.std.PlaneStats(None, 0)
 
-            if complexpr_available:
-                a = norm_expr(
-                    [cov_Ip, weight_in, weight, var_I],
-                    'x {thr} 1 1 1 -4 y.PlaneStatsMin y.PlaneStatsAverage 1e-6 - - / '
-                    'y y.PlaneStatsAverage - * exp + / - * z / + a {thr} z / + /',
-                    planes, thr=thr
-                )
-            else:
-                def _gradient(n: int, f: vs.VideoFrame) -> vs.VideoNode:
-                    frameMean = get_prop(f, 'PlaneStatsAverage', int, float)
-
-                    return norm_expr(
-                        [cov_Ip, weight_in, weight, var_I],
-                        'x {thr} 1 1 1 {kk} y {alpha} - * exp + / - * z / + a {thr} z / + /',
-                        planes, thr=thr, alpha=frameMean,
-                        kk=-4 / (get_prop(f, 'PlaneStatsMin', int, float) - frameMean - 1e-6)
-                    )
-
-                a = weight.std.FrameEval(_gradient, weight_in)
+            a = norm_expr(
+                [cov_Ip, weight_in, weight, var_I],
+                'x {thr} 1 1 1 -4 y.PlaneStatsMin y.PlaneStatsAverage 1e-6 - - / '
+                'y y.PlaneStatsAverage - * exp + / - * z / + a {thr} z / + /',
+                planes, thr=thr
+            )
 
     b = norm_expr([mean_p, a, mean_I], 'x y z * -', planes)
 

vsdeband/funcs.py
@@ -10,6 +10,7 @@ from vsrgtools import (
     BlurMatrix, MeanMode, RemoveGrainMode, RemoveGrainModeT,
     box_blur, gauss_blur, limit_filter, remove_grain
 )
+from vsexprtools import norm_expr
 from vstools import (
     ColorRange, PlanesT, VSFunction, FunctionUtil, check_ref_clip, check_variable, depth,
      expect_bits, fallback, normalize_planes, normalize_seq, scale_value, to_arr, vs
@@ -224,6 +225,7 @@ def multi_deband(
             ]).resize.Bicubic(format=vs.YUV444P16), split_planes=True
         )
         textures = box_blur(ExprOp.SUB(textures, edges), 2)
+        textures = norm_expr(textures, 'x 2 *')
 
     line_big = ExprOp.ADD(
         edgemask, gauss_blur(edgemask.std.Maximum(), 0.75), expr_suffix='4 *'
@@ -262,6 +264,4 @@ def multi_deband(
         lowpass=lambda *args, **kwargs: lowpass_deband, **freq_merge_kwargs
     )
 
-    return deband.std.MaskedMerge(
-        clip.std.Merge(base_deband, 0.5), textures.std.Expr('x 2 *')
-    )
+    return deband.std.MaskedMerge(clip.std.Merge(base_deband, 0.5), textures)

vsdehalo/warp.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from math import sqrt
 from typing import Sequence
 
-from vsmasktools import EdgeDetect, EdgeDetectT, PrewittStd
+from vsmasktools import EdgeDetect, EdgeDetectT, PrewittStd, Morpho
 from vsrgtools import min_blur, remove_grain, repair, BlurMatrix, box_blur
+from vsexprtools import norm_expr
 from vstools import (
     DitherType, PlanesT, core, cround, disallow_variable_format, disallow_variable_resolution, depth_func,
-    get_peak_value, get_y, join, normalize_planes, padder, scale_mask, split, vs
+    get_peak_value, get_y, join, normalize_planes, padder, scale_mask, split, limiter, vs
 )
 
 __all__ = [
@@ -34,7 +35,6 @@ def edge_cleaner(
     work_clip, *chroma = split(clip) if planes == [0] else (clip, )
     assert work_clip.format
 
-    peak = get_peak_value(work_clip)
     is_float = work_clip.format.sample_type == vs.FLOAT
 
     if smode:
@@ -61,11 +61,13 @@ def edge_cleaner(
 
     y_mask = get_y(work_clip)
 
-    mask = edgemask.edgemask(y_mask).std.Expr(
-        f'x {scale_mask(4, 8, work_clip)} < 0 x '
-        f'{scale_mask(32, 8, work_clip)} > {peak} x ? ?'
-    ).std.InvertMask()
-    mask = box_blur(mask)
+    mask = norm_expr(
+        edgemask.edgemask(y_mask),
+        'x {c4} < 0 x {c32} > range_in_max x ? ?',
+        c4=scale_mask(4, 8, work_clip),
+        c32=scale_mask(32, 8, work_clip),
+    )
+    mask = box_blur(mask.std.InvertMask())
 
     final = work_clip.std.MaskedMerge(warped, mask)
 
@@ -84,9 +86,12 @@ def edge_cleaner(
                 0.35
             )
         )
-        sc4 = scale_mask(4, 8, work_clip)
-        sc16 = scale_mask(16, 8, work_clip)
-        mask = remove_grain(mask, 7).std.Expr(f'x {sc4} < 0 x {sc16} > {peak} x ? ?')
+        mask = norm_expr(
+            remove_grain(mask, 7),
+            'x {sc4} < 0 x {sc16} > range_in_max x ? ?',
+            sc4=scale_mask(4, 8, work_clip),
+            sc16=scale_mask(16, 8, work_clip),
+        )
 
         final = final.std.MaskedMerge(work_clip, mask)
 
@@ -142,16 +147,13 @@ def YAHR(
 
     y_mask = get_y(work_clip)
 
-    vEdge = core.std.Expr(
-        [y_mask, y_mask.std.Maximum().std.Maximum()],
-        f'y x - {8 * get_peak_value(y_mask) / 255} - 128 *'
-    )
+    vEdge = norm_expr([y_mask, Morpho.maximum(y_mask, iterations=2)], 'y x - 8 range_max * 255 / - 128 *')
 
     mask1 = vEdge.tcanny.TCanny(sqrt(expand * 2), mode=-1)
 
     mask2 = BlurMatrix.BINOMIAL()(vEdge, planes=planes).std.Invert()
 
-    mask = core.std.Expr([mask1, mask2], 'x 16 * y min 0 max 1 min' if is_float else 'x 16 * y min')
+    mask = limiter(norm_expr([mask1, mask2], 'x 16 * y min'))
 
     final = work_clip.std.MaskedMerge(yahr, mask, planes)
 

vsmasktools/edge/_3x3.py
@@ -6,7 +6,7 @@ import math
 from abc import ABC
 from typing import Any, NoReturn, Sequence
 
-from vsexprtools import ExprOp
+from vsexprtools import norm_expr, ExprOp
 from vstools import ColorRange, KwargsT, depth, get_depth, join, split, vs
 
 from ..morpho import Morpho
@@ -245,7 +245,7 @@ class FreyChen(MatrixEdgeDetect):
     def _merge_edge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode:
         M = 'x x * y y * + z z * + a a * +'
         S = f'b b * c c * + d d * + e e * + f f * + {M} +'
-        return vs.core.std.Expr(clips, f'{M} {S} / sqrt')
+        return norm_expr(clips, f'{M} {S} / sqrt')
 
     def _merge_ridge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode | NoReturn:
         raise NotImplementedError

vsmasktools/edge/_5x5.py
@@ -4,6 +4,7 @@ from abc import ABC
 from typing import Any, Sequence
 
 from vstools import ColorRange, depth, vs
+from vsexprtools import norm_expr
 
 from ._abstract import EdgeDetect, EuclideanDistance, Max, RidgeDetect, SingleMatrix
 
@@ -110,7 +111,7 @@ class DoG(EuclideanDistance, Matrix5x5):
         return depth(clip, input_bits, range_out=ColorRange.FULL, range_in=ColorRange.FULL)
 
     def _merge_edge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode:
-        return vs.core.std.Expr(clips, 'x y -')
+        return norm_expr(clips, 'x y -')
 
 
 class Farid(RidgeDetect, EuclideanDistance, Matrix5x5):

vsmasktools/edge/_abstract.py
@@ -8,7 +8,7 @@ from jetpytools import inject_kwargs_params
 from vsexprtools import ExprOp, ExprToken, norm_expr
 from vstools import (
     ColorRange, CustomRuntimeError, CustomValueError, DitherType, FuncExceptT, KwargsT, PlanesT, T,
-    check_variable, core, depth, get_lowest_values, get_peak_value, get_peak_values, get_subclasses,
+    check_variable, depth, get_lowest_values, get_peak_value, get_peak_values, get_subclasses,
     inject_self, join, normalize_planes, plane, scale_mask, vs
 )
 
@@ -343,7 +343,7 @@ class RidgeDetect(MatrixEdgeDetect):
         return self._mask(clip, lthr, hthr, multi, clamp, _Feature.RIDGE, planes, **kwargs)
 
     def _merge_ridge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode:
-        return core.std.Expr(clips, 'x 2 pow z 2 pow 4 * + x y * 2 * - y 2 pow + sqrt x y + + 0.5 *')
+        return norm_expr(clips, 'x 2 pow z 2 pow 4 * + x y * 2 * - y 2 pow + sqrt x y + + 0.5 *')
 
     def _preprocess(self, clip: vs.VideoNode) -> vs.VideoNode:
         if len(self.matrices[0]) > 9 or (self.mode_types and self.mode_types[0] != 's'):
@@ -370,7 +370,7 @@ class SingleMatrix(MatrixEdgeDetect, ABC):
 
 class EuclideanDistance(MatrixEdgeDetect, ABC):
     def _merge_edge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode:
-        return core.std.Expr(clips, 'x x * y y * + sqrt')
+        return norm_expr(clips, 'x x * y y * + sqrt')
 
     def _merge_ridge(self, clips: Sequence[vs.VideoNode]) -> vs.VideoNode | NoReturn:
         raise NotImplementedError

vsmasktools/hardsub.py
@@ -260,12 +260,6 @@ class HardsubLine(HardsubMask):
         uv_thr = uv_range * 0.8
         uvexpr = f'x {uv_abs} {uv_thr} < y {uv_abs} {uv_thr} < and 255 0 ?'
 
-        upper = scale_value(0.8, 32, clip)
-        lower = scale_value(0.2, 32, clip)
-        mindiff = y_range * 0.1
-
-        difexpr = f'x {upper} > x {lower} < or x y - abs {mindiff} > and 255 0 ?'
-
         right = core.resize.Point(clip, src_left=4)
 
         subedge = norm_expr(
@@ -277,7 +271,15 @@ class HardsubLine(HardsubMask):
         clip_y, ref_y = get_y(clip), depth(get_y(ref), clip)
 
         clips = [box_blur(clip_y), box_blur(ref_y)]
-        diff = core.std.Expr(clips, difexpr, vs.GRAY8).std.Maximum().std.Maximum()
+        diff = norm_expr(
+            clips,
+            'x {upper} > x {lower} < or x y - abs {mindiff} > and 255 0 ?',
+            upper=scale_value(0.8, 32, clip),
+            lower=scale_value(0.2, 32, clip),
+            mindiff=y_range * 0.1,
+            format=vs.GRAY8,
+        )
+        diff = Morpho.maximum(diff, iterations=2)
 
         mask: vs.VideoNode = core.misc.Hysteresis(subedge, diff)
         mask = iterate(mask, core.std.Maximum, expand_n)

commit c87339139b7dd679ba8de6c49700f9e876c404ab

LinearLightProcessing: fix wclip not called by expr
Bug introduced by https://github.com/Jaded-Encoding-Thaumaturgy/vs-jetpack/commit/d80ebec232f8fa4e8da2efd78802879961009a99

vskernels/util.py
@@ -176,6 +176,7 @@ class LinearLight:
                     )
 
                 wclip = norm_expr(
+                    wclip,
                     '{center} 1 {slope} / 1 x 0 max 1 min {scale} * {offset} + / 1 - log * -',
                     center=self.ll._scenter, slope=self.ll._sslope,
                     scale=self.ll._sscale, offset=self.ll._soffset,
@@ -201,6 +202,7 @@ class LinearLight:
 
             if self.ll.sigmoid:
                 processed = norm_expr(
+                    processed,
                     '1 1 {slope} {center} x 0 max 1 min - * exp + / {offset} - {scale} /',
                     slope=self.ll._sslope, center=self.ll._scenter,
                     offset=self.ll._soffset, scale=self.ll._sscale,