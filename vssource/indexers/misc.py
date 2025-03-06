50880375c018dc384b0b42da8f995d05edb68114
Vardë <Ichunjo@users.noreply.github.com>
2025-03-06 02:29:43 +0100

    BestSource: set default cachemode to 3 and add index progress in CLI (#35)

--- vssource/indexers/misc.py
+++ vssource/indexers/misc.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from vstools import core
+from typing import Any
+
+from jetpytools import CustomIntEnum
+
+from vstools import core, vs
 
 from .base import Indexer
 
@@ -21,6 +25,58 @@ class BestSource(Indexer):
     """BestSource indexer"""
 
     _source_func = core.lazy.bs.VideoSource
+
+    class CacheMode(CustomIntEnum):
+        """Cache mode"""
+
+        NEVER = 0
+        """Never read or write index to disk."""
+        CACHE_PATH = 1
+        """
+        Always try to read index but only write index to disk when it will make a noticeable difference
+        on subsequent runs and store index files in a subtree of *cachepath*.
+        """
+        CACHE_PATH_WRITE = 2
+        """
+        Always try to read and write index to disk and store index files in a subtree of *cachepath*.
+        """
+        ABSOLUTE = 3
+        """
+        Always try to read index but only write index to disk when it will make a noticeable difference
+        on subsequent runs and store index files in the absolute path in *cachepath*
+        with track number and index extension appended.
+        """
+        ABSOLUTE_WRITE = 4
+        """
+        Always try to read and write index to disk and store index files
+        in the absolute path in *cachepath* with track number and index extension appended.
+        """
+
+    def __init__(self, *, force: bool = True, **kwargs: Any) -> None:
+        kwargs.setdefault("cachemode", BestSource.CacheMode.ABSOLUTE)
+        super().__init__(force=force, **kwargs)
+
+        from logging import WARNING, getLogger
+
+        try:
+            from vspreview import is_preview
+        except ImportError:
+            def is_preview() -> bool:
+                return False
+
+        def handler_func_best_source(m_type: vs.MessageType, msg: str) -> None:
+            if all([
+                m_type == vs.MESSAGE_TYPE_INFORMATION,
+                msg.startswith("VideoSource "),
+                getLogger().level <= WARNING,
+                is_preview()
+            ]):
+                print(msg, end="\r")
+
+        self._log_handle = core.add_log_handler(handler_func_best_source)
+
+    def __del__(self) -> None:
+        core.remove_log_handler(self._log_handle)
 
 
 class IMWRI(Indexer):
