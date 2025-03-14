1014d7e696c64ff414a6f7e65fb31a94acccd69c
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-03-14 16:08:33 +0000

    ci: use Nix to install plugins

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "vs-jetpack devenv";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    vs-nix-overlay.url = "github:sgt0/vs-nix-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    vs-nix-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          vs-nix-overlay.overlays.default
+        ];
+      };
+    in {
+      devShells = {
+        default =
+          pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              python312
+              python312Packages.vapoursynth
+              uv
+
+              (vapoursynth.withPlugins [
+                vspkgs.vapoursynthPlugins.akarin_jet
+                vspkgs.vapoursynthPlugins.resize2
+                vspkgs.vapoursynthPlugins.vszip
+              ])
+            ];
+          };
+      };
+    });
+}
