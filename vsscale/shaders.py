be92abff7db574865325c462fd82607a141fb314
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-04-03 03:56:21 +0200

    simplify PlaceboShader

--- vsscale/shaders.py
+++ vsscale/shaders.py
@@ -1,69 +1,57 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
 from math import ceil
-from pathlib import Path
-from typing import TYPE_CHECKING, Any, overload
+from typing import Any
 
-from vstools import (
-    MISSING, CustomRuntimeError, FileWasNotFoundError, MissingT, core, expect_bits, get_user_data_dir, get_video_format,
-    inject_self, join, vs
-)
+from jetpytools import SPath, SPathLike
 
-from .base import ShaderFileBase, ShaderFileCustom
+from vskernels import Catrom, KernelT, ScalerT
+from vstools import ConstantFormatVideoNode, check_variable, core, depth, inject_self, join, vs
+
 from .helpers import GenericScaler
 
 __all__ = [
     'PlaceboShader',
-
-    'ShaderFile'
 ]
 
 
-class PlaceboShaderMeta(GenericScaler):
-    shader_file: str | Path | ShaderFile
+class PlaceboShader(GenericScaler):
+    """Base placebo shader class."""
 
     _static_kernel_radius = 2
 
+    def __init__(
+        self,
+        shader: str | SPathLike,
+        *,
+        kernel: KernelT = Catrom,
+        scaler: ScalerT | None = None,
+        shifter: KernelT | None = None,
+        **kwargs: Any
+    ) -> None:
+        super().__init__(kernel=kernel, scaler=scaler, shifter=shifter, **kwargs)
 
-@dataclass
-class PlaceboShaderBase(PlaceboShaderMeta):
-    """Base placebo shader class."""
+        self.shader = SPath(shader).resolve().to_str()
 
-    chroma_loc: int | None = field(default=None, kw_only=True)
-    matrix: int | None = field(default=None, kw_only=True)
-    trc: int | None = field(default=None, kw_only=True)
-    linearize: int | None = field(default=None, kw_only=True)
-    sigmoidize: int | None = field(default=None, kw_only=True)
-    sigmoid_center: float | None = field(default=None, kw_only=True)
-    sigmoid_slope: float | None = field(default=None, kw_only=True)
-    antiring: float | None = field(default=None, kw_only=True)
-    filter_shader: str | None = field(default=None, kw_only=True)
-    clamp: float | None = field(default=None, kw_only=True)
-    blur: float | None = field(default=None, kw_only=True)
-    taper: float | None = field(default=None, kw_only=True)
-    radius: float | None = field(default=None, kw_only=True)
-    param1: float | None = field(default=None, kw_only=True)
-    param2: float | None = field(default=None, kw_only=True)
+    @inject_self.cached
+    def scale(
+        self,
+        clip: vs.VideoNode,
+        width: int | None = None,
+        height: int | None = None,
+        shift: tuple[float, float] = (0, 0),
+        **kwargs: Any
+    ) -> ConstantFormatVideoNode:
+        assert check_variable(clip, self.__class__)
 
-    def __post_init__(self) -> None:
-        super().__post_init__()
-
-        if not hasattr(self, 'shader_file'):
-            raise CustomRuntimeError('You must specify a "shader_file"!', self.__class__)
-
-    @inject_self
-    def scale(  # type: ignore
-        self, clip: vs.VideoNode, width: int | None = None, height: int | None = None,
-        shift: tuple[float, float] = (0, 0), **kwargs: Any
-    ) -> vs.VideoNode:
         width, height = self._wh_norm(clip, width, height)
 
-        output, _ = expect_bits(clip, 16)
+        kwargs = self.kwargs | kwargs
 
-        fmt = get_video_format(output)
+        output = depth(clip, 16)
 
-        if fmt.num_planes == 1:
+        # Add fake chroma planes
+        if output.format.num_planes == 1:
             if width > output.width or height > output.height:
                 output = output.resize.Point(format=vs.YUV444P16)
             else:
@@ -72,87 +60,20 @@ class PlaceboShaderBase(PlaceboShaderMeta):
                         blank = core.std.BlankClip(output, output.width // div, output.height // div, vs.GRAY16)
                         break
                 else:
-                    blank = output.std.BlankClip(vs.GRAY16)
+                    blank = core.std.BlankClip(output, format=vs.GRAY16)
 
                 output = join(output, blank, blank)
 
-        kwargs |= {
-            'shader': str(
-                self.shader_file()
-                if isinstance(self.shader_file, ShaderFile) else
-                ShaderFile.CUSTOM(self.shader_file)  # type: ignore
-            ),
-            'chroma_loc': self.chroma_loc, 'matrix': self.matrix,
-            'trc': self.trc, 'linearize': self.linearize,
-            'sigmoidize': self.sigmoidize, 'sigmoid_center': self.sigmoid_center, 'sigmoid_slope': self.sigmoid_slope,
-            'antiring': self.antiring, 'filter': self.filter_shader, 'clamp': self.clamp,
-            'blur': self.blur, 'taper': self.taper, 'radius': self.radius,
-            'param1': self.param1, 'param2': self.param2,
-        } | kwargs | {
-            'width': output.width * ceil(width / output.width),
-            'height': output.height * ceil(height / output.height)
-        }
+        # Configure filter param mainly used for chroma planes if input clip is GRAY. Box was slightly faster.
+        if 'filter' not in kwargs:
+            kwargs['filter'] = 'box' if output.format.num_planes == 1 else 'ewa_lanczos'
 
-        if not kwargs['filter']:
-            kwargs['filter'] = 'box' if fmt.num_planes == 1 else 'ewa_lanczos'
-
-        if not Path(kwargs['shader']).exists():
-            try:
-                kwargs['shader'] = str(ShaderFile.CUSTOM(kwargs['shader']))  # type: ignore
-            except FileWasNotFoundError:
-                ...
-
-        output = output.placebo.Shader(**kwargs)
+        output = core.placebo.Shader(
+            output,
+            self.shader,
+            output.width * ceil(width / output.width),
+            output.height * ceil(height / output.height),
+            **kwargs
+        )
 
         return self._finish_scale(output, clip, width, height, shift)
-
-
-@dataclass
-class PlaceboShader(PlaceboShaderBase):
-    shader_file: str | Path
-
-
-class ShaderFile(ShaderFileBase):
-    """Default shader files shipped with vsscale."""
-
-    if not TYPE_CHECKING:
-        CUSTOM = 'custom'
-
-    @overload
-    def __call__(self) -> Path:
-        ...
-
-    @overload
-    def __call__(self: ShaderFileCustom, file_name: str | Path) -> Path:  # type: ignore
-        ...
-
-    def __call__(self, file_name: str | Path | MissingT = MISSING) -> Path:
-        """Get a path from the shader member, name or path."""
-
-        if self is not ShaderFile.CUSTOM:
-            file_name = self.value
-
-        if file_name is MISSING:
-            raise TypeError("ShaderFile.__call__() missing 1 required positional argument: 'file_name'")
-
-        file_name, cwd = Path(file_name), Path.cwd()
-
-        assets_dirs = [
-            file_name,
-            cwd / file_name,
-            cwd / '.shaders' / file_name,
-            cwd / '_shaders' / file_name,
-            cwd / '.assets' / file_name,
-            cwd / '_assets' / file_name
-        ]
-
-        for asset_dir in assets_dirs:
-            if asset_dir.is_file():
-                return asset_dir
-
-        mpv_dir = get_user_data_dir().parent / 'Roaming' / 'mpv' / 'shaders' / file_name
-
-        if mpv_dir.is_file():
-            return mpv_dir
-
-        raise FileWasNotFoundError(f'"{file_name}" could not be found!', str(ShaderFile.CUSTOM))
