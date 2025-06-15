d4a779529a6541a11e270edd143119b7071ec5c7
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-06-15 02:45:59 +0200

    BestSource: fix index progress in CLI

--- vssource/indexers/misc.py
+++ vssource/indexers/misc.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from functools import cache
 from typing import Any
 
 from jetpytools import CustomIntEnum
@@ -21,6 +22,28 @@ __all__ = [
 ]
 
 
+@cache
+def _add_handler_func_bs() -> None:
+    from logging import WARNING, getLogger
+
+    try:
+        from vspreview import is_preview
+    except ImportError:
+        def is_preview() -> bool:
+            return False
+
+    def handler_func_best_source(m_type: vs.MessageType, msg: str) -> None:
+        if all([
+            m_type == vs.MESSAGE_TYPE_INFORMATION,
+            msg.startswith("VideoSource "),
+            getLogger().level <= WARNING,
+            is_preview()
+        ]):
+            print(msg, end="\r")
+
+    core.add_log_handler(handler_func_best_source)
+
+
 class BestSource(Indexer):
     """BestSource indexer"""
 
@@ -31,21 +54,25 @@ class BestSource(Indexer):
 
         NEVER = 0
         """Never read or write index to disk."""
+
         CACHE_PATH = 1
         """
         Always try to read index but only write index to disk when it will make a noticeable difference
         on subsequent runs and store index files in a subtree of *cachepath*.
         """
+
         CACHE_PATH_WRITE = 2
         """
         Always try to read and write index to disk and store index files in a subtree of *cachepath*.
         """
+
         ABSOLUTE = 3
         """
         Always try to read index but only write index to disk when it will make a noticeable difference
         on subsequent runs and store index files in the absolute path in *cachepath*
         with track number and index extension appended.
         """
+
         ABSOLUTE_WRITE = 4
         """
         Always try to read and write index to disk and store index files
@@ -53,34 +80,9 @@ class BestSource(Indexer):
         """
 
     def __init__(self, *, force: bool = True, cachemode: CacheMode = CacheMode.ABSOLUTE, **kwargs: Any) -> None:
+        _add_handler_func_bs()
+
         super().__init__(force=force, cachemode=cachemode, **kwargs)
-
-    def _source(self, *args: Any, **kwargs: Any) -> vs.VideoNode:
-
-        from logging import WARNING, getLogger
-
-        try:
-            from vspreview import is_preview
-        except ImportError:
-            def is_preview() -> bool:
-                return False
-
-        def handler_func_best_source(m_type: vs.MessageType, msg: str) -> None:
-            if all([
-                m_type == vs.MESSAGE_TYPE_INFORMATION,
-                msg.startswith("VideoSource "),
-                getLogger().level <= WARNING,
-                is_preview()
-            ]):
-                print(msg, end="\r")
-
-        logger = core.add_log_handler(handler_func_best_source)
-
-        clip = super()._source(*args, **kwargs)
-
-        core.remove_log_handler(logger)
-
-        return clip
 
 
 class IMWRI(Indexer):
