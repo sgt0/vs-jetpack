888cca841e2cc684cc6a7754060818cb911b042f
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-07-02 05:01:56 +0200

    based_aa: fix docstring

--- vsaa/funcs.py
+++ vsaa/funcs.py
@@ -172,8 +172,6 @@ def based_aa(
                                   The supersampler should ideally be fairly sharp without
                                   introducing too much ringing.
                                   Default: ArtCNN (R8F64).
-                                  If True, both fields will be processed separately, which may improve
-                                  anti-aliasing strength at the cost of increased processing time and detail loss.
                                   Default: False.
     :param antialiaser:           Antialiaser used for anti-aliasing. If None, EEDI3 will be selected with these default settings:
                                   (alpha=0.125, beta=0.25, vthresh0=12, vthresh1=24, field=1).
