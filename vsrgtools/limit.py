3b253a9057f81884dc142eabca2e993dc1946075
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2025-07-24 14:43:12 -0400

    limit_filter: code simplification

--- vsrgtools/limit.py
+++ vsrgtools/limit.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Sequence
 
 from vsexprtools import norm_expr
-from vstools import PlanesT, fallback, scale_delta, vs
+from vstools import PlanesT, fallback, scale_delta, to_arr, vs
 
 __all__ = ["limit_filter"]
 
@@ -12,8 +12,8 @@ def limit_filter(
     flt: vs.VideoNode,
     src: vs.VideoNode,
     ref: vs.VideoNode | None = None,
-    dark_thr: int | float | Sequence[int | float] = 1.0,
-    bright_thr: int | float | Sequence[int | float] = 1.0,
+    dark_thr: float | Sequence[float] = 1.0,
+    bright_thr: float | Sequence[float] = 1.0,
     elast: float | Sequence[float] = 2.0,
     planes: PlanesT = None,
 ) -> vs.VideoNode:
@@ -34,25 +34,19 @@ def limit_filter(
     """
     ref = fallback(ref, src)
 
-    if not isinstance(dark_thr, Sequence):
-        dark_thr = [dark_thr]
-
-    if not isinstance(bright_thr, Sequence):
-        bright_thr = [bright_thr]
-
     base_expr = "x y - DIFF! x z - abs DIFF_REF_ABS!"
     dark_expr = (
-        "{dark_thr} {elast} * THR2! 1 THR2@ {dark_thr} - / SLOPE! "
-        "DIFF_REF_ABS@ {dark_thr} <= x DIFF_REF_ABS@ THR2@ >= y "
-        "y DIFF@ THR2@ DIFF_REF_ABS@ - * SLOPE@ * + ? ?"
+        "{dark_thr} {elast} * THR! 1 THR@ {dark_thr} - / SLOPE! "
+        "DIFF_REF_ABS@ {dark_thr} <= x DIFF_REF_ABS@ THR@ >= y "
+        "y DIFF@ THR@ DIFF_REF_ABS@ - * SLOPE@ * + ? ?"
     )
     bright_expr = dark_expr.replace("dark_thr", "bright_thr")
 
     return norm_expr(
         [flt, src, ref],
         f"{base_expr} x z < {dark_expr} {bright_expr} ?",
-        dark_thr=[scale_delta(x, 8, flt) for x in dark_thr],
-        bright_thr=[scale_delta(x, 8, flt) for x in bright_thr],
+        dark_thr=[scale_delta(x, 8, flt) for x in to_arr(dark_thr)],
+        bright_thr=[scale_delta(x, 8, flt) for x in to_arr(bright_thr)],
         elast=elast,
         planes=planes,
     )
