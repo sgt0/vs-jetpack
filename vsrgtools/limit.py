bb4eead6388d9b50df051cb28758953b9ae121c9
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2025-07-24 14:35:52 -0400

    limit_filter: use more accurate threshold scaling

--- vsrgtools/limit.py
+++ vsrgtools/limit.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Sequence
 
 from vsexprtools import norm_expr
-from vstools import PlanesT, fallback, vs
+from vstools import PlanesT, fallback, scale_delta, vs
 
 __all__ = ["limit_filter"]
 
@@ -12,8 +12,8 @@ def limit_filter(
     flt: vs.VideoNode,
     src: vs.VideoNode,
     ref: vs.VideoNode | None = None,
-    dark_thr: float | Sequence[float] = 1.0,
-    bright_thr: float | Sequence[float] = 1.0,
+    dark_thr: int | float | Sequence[int | float] = 1.0,
+    bright_thr: int | float | Sequence[int | float] = 1.0,
     elast: float | Sequence[float] = 2.0,
     planes: PlanesT = None,
 ) -> vs.VideoNode:
@@ -34,11 +34,16 @@ def limit_filter(
     """
     ref = fallback(ref, src)
 
+    if not isinstance(dark_thr, Sequence):
+        dark_thr = [dark_thr]
+
+    if not isinstance(bright_thr, Sequence):
+        bright_thr = [bright_thr]
+
     base_expr = "x y - DIFF! x z - abs DIFF_REF_ABS!"
     dark_expr = (
-        "{dark_thr} range_max 255 / * THR1! THR1@ {elast} * THR2! "
-        "1 THR2@ THR1@ - / SLOPE! "
-        "DIFF_REF_ABS@ THR1@ <= x DIFF_REF_ABS@ THR2@ >= y "
+        "{dark_thr} {elast} * THR2! 1 THR2@ {dark_thr} - / SLOPE! "
+        "DIFF_REF_ABS@ {dark_thr} <= x DIFF_REF_ABS@ THR2@ >= y "
         "y DIFF@ THR2@ DIFF_REF_ABS@ - * SLOPE@ * + ? ?"
     )
     bright_expr = dark_expr.replace("dark_thr", "bright_thr")
@@ -46,8 +51,8 @@ def limit_filter(
     return norm_expr(
         [flt, src, ref],
         f"{base_expr} x z < {dark_expr} {bright_expr} ?",
-        dark_thr=dark_thr,
-        bright_thr=bright_thr,
+        dark_thr=[scale_delta(x, 8, flt) for x in dark_thr],
+        bright_thr=[scale_delta(x, 8, flt) for x in bright_thr],
         elast=elast,
         planes=planes,
     )
