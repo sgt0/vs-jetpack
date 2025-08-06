1436918382dc70a616405b822d370895e60fbd4b
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-08-06 19:11:40 +0200

    BestSource: fix logging - fix logging not displayed outside of previewing - fix last displayed message overlapping the next message displayed

--- vssource/indexers/misc.py
+++ vssource/indexers/misc.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from functools import cache
 from typing import Any
 
-from jetpytools import CustomIntEnum
+from jetpytools import CustomIntEnum, DataType, SPathLike
 
 from vstools import core, vs
 
@@ -12,27 +12,17 @@ from .base import Indexer
 __all__ = ["FFMS2", "IMWRI", "LSMAS", "BestSource", "CarefulSource"]
 
 
+_bs_msgs = set[str]()
+
+
 @cache
 def _add_handler_func_bs() -> None:
     from logging import WARNING, getLogger
 
-    try:
-        from vspreview import is_preview
-    except ImportError:
-
-        def is_preview() -> bool:
-            return False
-
     def handler_func_best_source(m_type: vs.MessageType, msg: str) -> None:
-        if all(
-            [
-                m_type == vs.MESSAGE_TYPE_INFORMATION,
-                msg.startswith("VideoSource "),
-                getLogger().level <= WARNING,
-                is_preview(),
-            ]
-        ):
-            print(msg, end="\r")
+        if all([m_type == vs.MESSAGE_TYPE_INFORMATION, msg.startswith("VideoSource "), getLogger().level <= WARNING]):
+            _bs_msgs.add(msg)
+            print(msg, end="\r", flush=True)
 
     core.add_log_handler(handler_func_best_source)
 
@@ -82,6 +72,18 @@ class BestSource(Indexer):
         _add_handler_func_bs()
 
         super().__init__(force=force, cachemode=cachemode, **kwargs)
+
+    @classmethod
+    def source_func(cls, path: DataType | SPathLike, *args: Any, **kwargs: Any) -> vs.VideoNode:
+        _bs_msgs.clear()
+
+        clip = super().source_func(path, *args, **kwargs)
+
+        if _bs_msgs:
+            print(flush=True)
+            _bs_msgs.clear()
+
+        return clip
 
 
 class IMWRI(Indexer):
