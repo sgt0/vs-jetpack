10b8001e51c971a0a40c85c48e8c1f26ee3b4521
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-09-06 00:22:06 +0200

    remove f3kdb submodule

--- vsdeband/__init__.py
+++ vsdeband/__init__.py
@@ -4,7 +4,6 @@ A collection of wrappers and functions for debanding.
 
 from .abstract import *
 from .debanders import *
-from .f3kdb import *
 from .mask import *
 from .noise import *
 from .placebo import *
