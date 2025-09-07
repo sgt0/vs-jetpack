f5ab789668fb968cd8629cafff6084dfbc164f61
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-09-07 03:25:53 +0200

    vsexprtools: move extra_op_tokenize_expr, bitdepth_aware_tokenize_expr, norm_expr_planes to funcs submodule

--- vsexprtools/funcs.py
+++ vsexprtools/funcs.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+import re
 from math import ceil
-from typing import TYPE_CHECKING, Any, Iterable, Sequence, SupportsIndex, TypeAlias, Union
+from typing import TYPE_CHECKING, Any, Callable, Iterable, Sequence, SupportsIndex, TypeAlias, Union
+
+from jetpytools import CustomIndexError
 
 from vstools import (
+    EXPR_VARS,
+    ColorRange,
     ConstantFormatVideoNode,
     CustomRuntimeError,
     FuncExcept,
@@ -18,13 +23,15 @@ from vstools import (
     core,
     flatten_vnodes,
     get_video_format,
+    normalize_planes,
+    normalize_seq,
     to_arr,
     vs,
 )
 
 from .error import CustomExprError
 from .exprop import ExprList, ExprOp, ExprOpBase, TupleExprList
-from .util import ExprVars, bitdepth_aware_tokenize_expr, norm_expr_planes
+from .util import ExprVars
 
 __all__ = ["combine", "combine_expr", "expr_func", "norm_expr"]
 
@@ -256,3 +263,83 @@ def norm_expr(
         print(tokenized_expr)
 
     return expr_func(clips, tokenized_expr, format, opt, boundary, func)
+
+
+def extra_op_tokenize_expr(expr: str) -> str:
+    # Workaround for the not implemented op
+    from .exprop import ExprOp
+
+    def _replace_polyval(matched: re.Match[str]) -> str:
+        degree = int(matched.group(1))
+        return ExprOp.POLYVAL.convert_extra(degree)
+
+    for extra_op in ExprOp._extra_op_names_:
+        if extra_op.lower() in expr:
+            if extra_op == ExprOp.POLYVAL.name:
+                expr = re.sub(rf"\b{extra_op.lower()}\b", _replace_polyval, expr)
+            else:
+                expr = re.sub(rf"\b{extra_op.lower()}\b", getattr(ExprOp, extra_op).convert_extra(), expr)
+
+    return expr
+
+
+def bitdepth_aware_tokenize_expr(
+    clips: Sequence[vs.VideoNode], expr: str, chroma: bool, func: FuncExcept | None = None
+) -> str:
+    from .exprop import ExprToken
+
+    func = func or bitdepth_aware_tokenize_expr
+
+    expr = extra_op_tokenize_expr(expr)
+
+    if not expr or len(expr) < 4:
+        return expr
+
+    replaces = list[tuple[str, Callable[[vs.VideoNode, bool, ColorRange], float]]]()
+
+    for token in sorted(ExprToken, key=lambda x: len(x), reverse=True):
+        if token.value in expr:
+            replaces.append((token.value, token.get_value))
+
+        if token.name in expr:
+            replaces.append((f"{token.__class__.__name__}.{token.name}", token.get_value))
+
+    if not replaces:
+        return expr
+
+    clips = list(clips)
+    ranges = [ColorRange.from_video(c, func=func) for c in clips]
+
+    mapped_clips = list(reversed(list(zip(["", *EXPR_VARS], clips[:1] + clips, ranges[:1] + ranges))))
+
+    for mkey, function in replaces:
+        if mkey in expr:
+            for key, clip, crange in [
+                (f"{mkey}_{k}" if k else f"{mkey}", clip, crange) for k, clip, crange in mapped_clips
+            ]:
+                expr = re.sub(rf"\b{key}\b", str(function(clip, chroma, crange)), expr)
+
+        if re.search(rf"\b{mkey}\b", expr):
+            raise CustomIndexError("Parsing error or not enough clips passed!", func, reason=expr)
+
+    return expr
+
+
+def norm_expr_planes(
+    clip: vs.VideoNode,
+    expr: str | list[str],
+    planes: Planes = None,
+    **kwargs: Iterable[SupportsString] | SupportsString,
+) -> list[str]:
+    assert clip.format
+
+    expr_array = normalize_seq(expr, clip.format.num_planes)
+
+    planes = normalize_planes(clip, planes)
+
+    string_args = [(key, normalize_seq(value)) for key, value in kwargs.items()]
+
+    return [
+        exp.format(**({"plane_idx": i} | {key: value[i] for key, value in string_args})) if i in planes else ""
+        for i, exp in enumerate(expr_array)
+    ]
