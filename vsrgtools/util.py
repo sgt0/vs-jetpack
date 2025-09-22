f8e9d9059202942df37ac40ddf1131465db9bc85
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-09-23 00:26:16 +0200

    normalize_radius: mangle arguments to make them unique

--- vsrgtools/util.py
+++ vsrgtools/util.py
@@ -18,27 +18,28 @@ __all__ = ["normalize_radius"]
 
 
 def normalize_radius(
-    clip: vs.VideoNode,
-    function: GenericVSFunction[ConstantFormatVideoNode],
-    radius: Sequence[float | int] | dict[str, Sequence[float | int]],
+    __clip: vs.VideoNode,
+    __function: GenericVSFunction[ConstantFormatVideoNode],
+    __radius: Sequence[float | int] | dict[str, Sequence[float | int]],
+    /,
     planes: Planes,
     **kwargs: Any,
 ) -> ConstantFormatVideoNode:
-    assert check_variable_format(clip, normalize_radius)
+    assert check_variable_format(__clip, normalize_radius)
 
-    if isinstance(radius, dict):
-        name, radius = radius.popitem()
+    if isinstance(__radius, dict):
+        name, radius = __radius.popitem()
     else:
-        name, radius = "radius", radius
+        name, radius = "radius", __radius
 
-    radius = normalize_seq(radius, clip.format.num_planes)
-    planes = normalize_planes(clip, planes)
+    radius = normalize_seq(radius, __clip.format.num_planes)
+    planes = normalize_planes(__clip, planes)
 
     if len(set(radius)) > 1:
         pplanes = [
-            function(p, **kwargs | {name: rad, "planes": 0}) if i in planes else p
-            for i, (rad, p) in enumerate(zip(radius, split(clip)))
+            __function(p, **kwargs | {name: rad, "planes": 0}) if i in planes else p
+            for i, (rad, p) in enumerate(zip(radius, split(__clip)))
         ]
         return join(pplanes)
 
-    return function(clip, **kwargs | {name: radius[0], "planes": planes})
+    return __function(__clip, **kwargs | {name: radius[0], "planes": planes})
