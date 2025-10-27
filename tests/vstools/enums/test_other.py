481e5974c4aeaff932716dc99a0fda85332afe6c
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-10-27 21:27:58 +0100

    remove Direction & Region tests

--- tests/vstools/enums/test_other.py
+++ tests/vstools/enums/test_other.py
@@ -1,26 +1,7 @@
-from fractions import Fraction
 from unittest import TestCase
 
 from vstools import get_prop, vs
-from vstools.enums.other import Dar, Direction, Region, Resolution, Sar
-
-
-class TestDirection(TestCase):
-    def test_is_axis(self) -> None:
-        self.assertTrue(Direction.HORIZONTAL.is_axis)
-        self.assertTrue(Direction.VERTICAL.is_axis)
-        self.assertFalse(Direction.LEFT.is_axis)
-        self.assertFalse(Direction.RIGHT.is_axis)
-        self.assertFalse(Direction.UP.is_axis)
-        self.assertFalse(Direction.DOWN.is_axis)
-
-    def test_is_way(self) -> None:
-        self.assertFalse(Direction.HORIZONTAL.is_way)
-        self.assertFalse(Direction.VERTICAL.is_way)
-        self.assertTrue(Direction.LEFT.is_way)
-        self.assertTrue(Direction.RIGHT.is_way)
-        self.assertTrue(Direction.UP.is_way)
-        self.assertTrue(Direction.DOWN.is_way)
+from vstools.enums.other import Dar, Resolution, Sar
 
 
 class TestDar(TestCase):
@@ -46,26 +27,6 @@ class TestSar(TestCase):
         clip = Sar(1920, 1).apply(clip)
         self.assertEqual(get_prop(clip, "_SARNum", int), 1920)
         self.assertEqual(get_prop(clip, "_SARDen", int), 1)
-
-
-class TestRegion(TestCase):
-    def test_framerate(self) -> None:
-        self.assertEqual(Region.UNKNOWN.framerate, Fraction(0))
-        self.assertEqual(Region.NTSC.framerate, Fraction(30000, 1001))
-        self.assertEqual(Region.NTSCi.framerate, Fraction(60000, 1001))
-        self.assertEqual(Region.PAL.framerate, Fraction(25, 1))
-        self.assertEqual(Region.PALi.framerate, Fraction(50, 1))
-        self.assertEqual(Region.FILM.framerate, Fraction(24, 1))
-        self.assertEqual(Region.NTSC_FILM.framerate, Fraction(24000, 1001))
-
-    def test_from_framerate(self) -> None:
-        self.assertEqual(Region.from_framerate(Fraction(0)), Region.UNKNOWN)
-        self.assertEqual(Region.from_framerate(Fraction(30000, 1001)), Region.NTSC)
-        self.assertEqual(Region.from_framerate(Fraction(60000, 1001)), Region.NTSCi)
-        self.assertEqual(Region.from_framerate(Fraction(25, 1)), Region.PAL)
-        self.assertEqual(Region.from_framerate(Fraction(50, 1)), Region.PALi)
-        self.assertEqual(Region.from_framerate(Fraction(24, 1)), Region.FILM)
-        self.assertEqual(Region.from_framerate(Fraction(24000, 1001)), Region.NTSC_FILM)
 
 
 class TestResolution(TestCase):
