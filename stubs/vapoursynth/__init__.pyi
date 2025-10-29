3e26aca444553a3f014ad2cbf9fe321732eb22d9
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-10-29 16:33:03 +0100

    stubs: replace int with Literal for VideoFormat fields

--- stubs/vapoursynth/__init__.pyi
+++ stubs/vapoursynth/__init__.pyi
@@ -682,22 +682,26 @@ class _VideoFormatDict(TypedDict):
     name: str
     color_family: ColorFamily
     sample_type: SampleType
-    bits_per_sample: int
+    bits_per_sample: Literal[
+        8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
+    ]
     bytes_per_sample: int
-    subsampling_w: int
-    subsampling_h: int
-    num_planes: int
+    subsampling_w: Literal[0, 1, 2, 3, 4]
+    subsampling_h: Literal[0, 1, 2, 3, 4]
+    num_planes: Literal[1, 3]
 
 class VideoFormat:
     id: Final[int]
     name: Final[str]
     color_family: Final[ColorFamily]
     sample_type: Final[SampleType]
-    bits_per_sample: Final[int]
+    bits_per_sample: Final[
+        Literal[8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+    ]
     bytes_per_sample: Final[int]
-    subsampling_w: Final[int]
-    subsampling_h: Final[int]
-    num_planes: Final[int]
+    subsampling_w: Final[Literal[0, 1, 2, 3, 4]]
+    subsampling_h: Final[Literal[0, 1, 2, 3, 4]]
+    num_planes: Final[Literal[1, 3]]
     def __repr__(self) -> str: ...
     def __str__(self) -> str: ...
     def __eq__(self, other: object) -> bool: ...
