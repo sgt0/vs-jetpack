sgt0/vs-jetpack

commit b38f5f4ea3edc49cd106b70316cd888905f2bc87

Deinterlacer: allow None as valid value
Squashed commit of the following:

commit 241a85147e9519027458c20a2106e65c29e028f6
Author: emotion3459 <176516814+emotion3459@users.noreply.github.com>
Date:   Fri Nov 7 17:08:27 2025 -0500

    force aa dh to false

commit a13b0b8b4a13000bac9b68cf6fdbf0c5a399327c
Author: emotion3459 <176516814+emotion3459@users.noreply.github.com>
Date:   Fri Nov 7 16:59:26 2025 -0500

    fix kwargs getter, use _interpolate for aa

commit 1cbde8d16bf45415b636c345242538622a2bf854
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 21:31:22 2025 +0100

    Deinterlacer._interpolate remove None as possible value

commit 2d5694d3e108b0dbb19b794cb863690e3b82ced5
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 21:28:44 2025 +0100

    FieldBased.is_tff: throw error if PROGRESSIVE

commit e9dbcbcc9601592b50f56263332dc2e86251eac0
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 20:32:42 2025 +0100

    adjust logic again

commit 8b084152b6368531be668dc28ca46727c21bf27c
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 20:24:43 2025 +0100

    Revert "adjust logic²"

    This reverts commit 9ce525358986adcf0dbf170e1bc9701e763c13df.

commit 9ce525358986adcf0dbf170e1bc9701e763c13df
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 20:22:14 2025 +0100

    adjust logic²

commit a37d36b9c950ef1b450e774e96c1b10fceabba04
Author: Ichunjo <ichunjo.le.terrible@gmail.com>
Date:   Fri Nov 7 20:12:45 2025 +0100

    get tff from kwargs if available

commit 686d1b6308503153e3c4307c2c174ad599cf141d
Author: emotion3459 <176516814+emotion3459@users.noreply.github.com>
Date:   Fri Nov 7 13:39:01 2025 -0500

    add back tff fallbacks

commit 8b22eda9ebfca699f5ceccc2baff9867deccbc78
Author: emotion3459 <176516814+emotion3459@users.noreply.github.com>
Date:   Fri Nov 7 13:36:26 2025 -0500

    adjust logic

commit 01a4044e14ce0c9b3ceb33c94a05d9451160cc34

vsaa/deinterlacers.py
@@ -19,15 +19,7 @@ from vskernels import (
     Scaler,
     TopShift,
 )
-from vstools import (
-    ChromaLocation,
-    FieldBased,
-    FieldBasedLike,
-    VSFunctionAllArgs,
-    VSFunctionNoArgs,
-    core,
-    vs,
-)
+from vstools import ChromaLocation, FieldBased, FieldBasedLike, VSFunctionAllArgs, VSFunctionNoArgs, core, vs
 
 __all__ = [
     "BWDIF",
@@ -79,7 +71,7 @@ class Deinterlacer(Bobber, ABC):
     Abstract base class for deinterlacing operations.
     """
 
-    tff: bool = False
+    tff: bool | None = None
     """The field order."""
 
     double_rate: bool = True
@@ -147,13 +139,9 @@ class Deinterlacer(Bobber, ABC):
         Returns:
             Deinterlaced clip.
         """
-        return self._interpolate(
-            clip,
-            FieldBased.from_param(fallback(tff, self.tff), self.__class__).is_tff(),
-            fallback(double_rate, self.double_rate),
-            False,
-            **kwargs,
-        )
+        field_based = FieldBased.from_param_or_video(fallback(tff, self.tff, default=None), clip, True, self.__class__)
+
+        return self._interpolate(clip, field_based.is_tff(), fallback(double_rate, self.double_rate), False, **kwargs)
 
     def bob(self, clip: vs.VideoNode, *, tff: FieldBasedLike | bool | None = None, **kwargs: Any) -> vs.VideoNode:
         """
@@ -167,9 +155,9 @@ class Deinterlacer(Bobber, ABC):
         Returns:
             Deinterlaced clip.
         """
-        return self._interpolate(
-            clip, FieldBased.from_param(fallback(tff, self.tff), self.__class__).is_tff(), True, False, **kwargs
-        )
+        field_based = FieldBased.from_param_or_video(fallback(tff, self.tff, default=None), clip, True, self.__class__)
+
+        return self._interpolate(clip, field_based.is_tff(), True, False, **kwargs)
 
     def copy(self, **kwargs: Any) -> Self:
         """
@@ -219,12 +207,14 @@ class AntiAliaser(Deinterlacer, ABC):
         Returns:
             Anti-aliased clip.
         """
+        tff = fallback(kwargs.pop("tff", self.tff), True)
+
         for y in sorted((aa_dir for aa_dir in self.AADirection), key=lambda x: x.value, reverse=self.transpose_first):
             if direction in (y, self.AADirection.BOTH):
                 if y == self.AADirection.HORIZONTAL:
                     clip = self.transpose(clip)
 
-                clip = self.deinterlace(clip, **kwargs)
+                clip = self._interpolate(clip, tff, dh=False, **kwargs)
 
                 if self.double_rate:
                     clip = core.std.Merge(clip[::2], clip[1::2])
@@ -287,6 +277,8 @@ class SuperSampler(Scaler, AntiAliaser, ABC):
         Returns:
             The scaled clip.
         """
+        tff_fallback = fallback(kwargs.pop("tff", self.tff), True)
+
         dims = self._wh_norm(clip, width, height)
         dest_dimensions = list(dims)
         sy, sx = shift
@@ -313,7 +305,7 @@ class SuperSampler(Scaler, AntiAliaser, ABC):
 
             while clip.height < dim:
                 delta = max(nshift[x], key=lambda y: abs(y))
-                tff = False if delta < 0 else True if delta > 0 else self.tff
+                tff = False if delta < 0 else True if delta > 0 else tff_fallback
                 offset = -0.25 if tff else 0.25
 
                 for y in range(clip.format.num_planes):
@@ -448,7 +440,7 @@ class NNEDI3(SuperSampler):
         return core.lazy.sneedif.NNEDI3 if self.opencl else core.lazy.znedi3.nnedi3
 
     def _interpolate(self, clip: vs.VideoNode, tff: bool, double_rate: bool, dh: bool, **kwargs: Any) -> vs.VideoNode:
-        field = int(tff) + int(double_rate) * 2
+        field = bool(tff) + int(double_rate) * 2
 
         return self._deinterlacer_function(clip, field, dh, **self.get_deint_args(**kwargs))
 
@@ -557,7 +549,7 @@ class EEDI2(SuperSampler):
         return core.lazy.eedi2cuda.EEDI2 if self.cuda else core.lazy.eedi2.EEDI2
 
     def _interpolate(self, clip: vs.VideoNode, tff: bool, double_rate: bool, dh: bool, **kwargs: Any) -> vs.VideoNode:
-        field = int(tff) + int(double_rate) * 2
+        field = bool(tff) + int(double_rate) * 2
 
         if not dh:
             clip = clip.std.SeparateFields(tff)
@@ -698,7 +690,7 @@ class EEDI3(SuperSampler):
         return core.lazy.eedi3m.EEDI3
 
     def _interpolate(self, clip: vs.VideoNode, tff: bool, double_rate: bool, dh: bool, **kwargs: Any) -> vs.VideoNode:
-        field = int(tff) + int(double_rate) * 2
+        field = bool(tff) + int(double_rate) * 2
 
         kwargs = self.get_deint_args(**kwargs)
 
@@ -818,7 +810,7 @@ class BWDIF(Deinterlacer):
         return core.lazy.bwdif.Bwdif
 
     def _interpolate(self, clip: vs.VideoNode, tff: bool, double_rate: bool, dh: bool, **kwargs: Any) -> vs.VideoNode:
-        field = int(tff) + int(double_rate) * 2
+        field = bool(tff) + int(double_rate) * 2
 
         if callable(self.edeint):
             kwargs.update(edeint=self.edeint(clip))

tests/vstools/enums/test_generic.py
@@ -95,7 +95,8 @@ class TestFieldBased(TestCase):
     def test_is_tff(self) -> None:
         self.assertTrue(FieldBased.TFF.is_tff())
         self.assertFalse(FieldBased.BFF.is_tff())
-        self.assertFalse(FieldBased.PROGRESSIVE.is_tff())
+        with self.assertRaises(UnsupportedFieldBasedError):
+            self.assertFalse(FieldBased.PROGRESSIVE.is_tff())
 
     def test_inverted(self) -> None:
         self.assertEqual(FieldBased.TFF.inverted_field, FieldBased.BFF)

vsdeinterlace/qtgmc.py
@@ -319,13 +319,14 @@ class QTempGaussMC(VSObject):
 
         self.clip = clip
         self.input_type = input_type
-        self.tff = clip_fieldbased.is_tff()
-        self.double_rate = self.input_type != self.InputType.REPAIR
 
         if self.input_type == self.InputType.PROGRESSIVE and clip_fieldbased.is_inter():
             raise UnsupportedFieldBasedError(f"{self.input_type} incompatible with interlaced video!", self.__class__)
         elif self.input_type in (self.InputType.INTERLACE, self.InputType.REPAIR) and not clip_fieldbased.is_inter():
             raise UnsupportedFieldBasedError(f"{self.input_type} incompatible with progressive video!", self.__class__)
+
+        self.tff = clip_fieldbased.is_tff()
+        self.double_rate = self.input_type != self.InputType.REPAIR
 
     def prefilter(
         self,

vstools/enums/generic.py
@@ -122,9 +122,7 @@ class FieldBased(PropEnum):
             UnsupportedFieldBasedError: If PROGRESSIVE value is passed.
         """
         if self is self.PROGRESSIVE:
-            raise UnsupportedFieldBasedError(
-                "Progressive video aren't field based!", f"{self.__class__.__name__}.field"
-            )
+            raise UnsupportedFieldBasedError("Progressive video isn't field based.", "FieldBased.field")
 
         return Field.from_param(self.value - 1)
 
@@ -134,12 +132,10 @@ class FieldBased(PropEnum):
         Get the inverted field order.
 
         Raises:
-            UnsupportedFieldBasedError: PROGRESSIVE value is passed.
+            UnsupportedFieldBasedError: If PROGRESSIVE value is passed.
         """
-        if self == self.PROGRESSIVE:
-            raise UnsupportedFieldBasedError(
-                "Progressive video aren't field based!", f"{self.__class__.__name__}.inverted_field"
-            )
+        if self is self.PROGRESSIVE:
+            raise UnsupportedFieldBasedError("Progressive video isn't field based.", "FieldBased.inverted_field")
 
         return FieldBased.BFF if self.is_tff() else FieldBased.TFF
 
@@ -159,7 +155,13 @@ class FieldBased(PropEnum):
     def is_tff(self) -> bool:
         """
         Check whether the value is Top-Field-First.
+
+        Raises:
+            UnsupportedFieldBasedError: If PROGRESSIVE value is passed
         """
+        if self is self.PROGRESSIVE:
+            raise UnsupportedFieldBasedError("Progressive video isn't field based.", self.is_tff)
+
         return self is self.TFF
 
     @classmethod

commit ce7e44cbc5a0946556a18e9332bd3d45581b28da

AntiAliaser: fix missing double_rate argument
Regression from https://github.com/Jaded-Encoding-Thaumaturgy/vs-jetpack/commit/b38f5f4ea3edc49cd106b70316cd888905f2bc87

vsaa/deinterlacers.py
@@ -214,7 +214,7 @@ class AntiAliaser(Deinterlacer, ABC):
                 if y == self.AADirection.HORIZONTAL:
                     clip = self.transpose(clip)
 
-                clip = self._interpolate(clip, tff, dh=False, **kwargs)
+                clip = self._interpolate(clip, tff, self.double_rate, False, **kwargs)
 
                 if self.double_rate:
                     clip = core.std.Merge(clip[::2], clip[1::2])